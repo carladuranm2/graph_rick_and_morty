bad4712a76c53e0489172b4676c24a5d36df4b17
carladuranm2 <82276399+carladuranm2@users.noreply.github.com>
2022-08-02 23:31:22 +0200

    fix: chage width

--- src/module/home/infrastructure/components/cardInfo/styled.js
+++ src/module/home/infrastructure/components/cardInfo/styled.js
@@ -5,5 +5,5 @@ export const WrapperCardInfo = styled.div`
   box-shadow: rgb(136 152 170 / 15%) 0px ${props => props.top ? '2rem' : "0px"} 2rem 0px;
   border-bottom: ${props => props.borderBottom ? '1px' : '0px'} solid rgb(236, 237, 239);
   padding: 20px;
-  width: 100%
+  width: ${props => props.width ? '100%' : ''}
 `;
